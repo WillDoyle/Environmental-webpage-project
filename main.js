255a8c7fc0070f33fce24d9f2435ab82be6cacc8
WillDoyle <64522578+WillDoyle@users.noreply.github.com>
2022-01-21 09:47:51 +1000

    javascript

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
-var elements = document.getElementsByClassName('rainforest__3--slide');
+var defaultImg = document.getElementById("default-image").innerHTML;
+var communitiesImg = document.getElementById("communities-image").innerHTML;
+var rainforestImg = document.getElementById("rainforest-image").innerHTML;
+var climateImg = document.getElementById("climate-image").innerHTML;
 
-
-function mouseEnter() {
-    $(document).ready(function(){
-           var opacity = $(".rainforest__3--slide").css("opacity");
-           console.log("opacity", opacity);
-           if (opacity > 0) {
-             $(".rainforest__3--slide").css("opacity", 0);
-           }
-           else if (opacity == 0) {
-             $(".rainforest__3--slide").css("opacity", 1);
-           }
-       });
-    }
-            for (i = 0; i < elements.length; i++) {
-                elements[i].addEventListener("mouseenter", mouseEnter);
-            }
-
-            //hover over slide element
-            //change slide background
+defaultImg.style.opacity = "0.5";
+rainforestImg.style.opacity = "0.5";
+communitiesImg.style.opacity = "0.5";
+climateImg.style.opacity = "0.5";
