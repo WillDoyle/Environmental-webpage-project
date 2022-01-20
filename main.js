2eb33e908c1d49122c01a43f7e58c5477aa5a83c
WillDoyle <64522578+WillDoyle@users.noreply.github.com>
2022-01-20 16:08:45 +1000

    javascript update

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+var elements = document.getElementsByClassName('rainforest__3--slide');
 
-var element = document.getElementsByClassName('communities rainforest climate-action')
 
+function mouseEnter() {
+    $(document).ready(function(){
+           var opacity = $(".rainforest__3--slide").css("opacity");
+           console.log("opacity", opacity);
+           if (opacity > 0) {
+             $(".rainforest__3--slide").css("opacity", 0);
+           }
+           else if (opacity == 0) {
+             $(".rainforest__3--slide").css("opacity", 1);
+           }
+       });
+    }
+            for (i = 0; i < elements.length; i++) {
+                elements[i].addEventListener("mouseenter", mouseEnter);
+            }
 
-function mouseEnter(){
-    
-}
+            //hover over slide element
+            //change slide background
