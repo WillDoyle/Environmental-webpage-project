617b9b31d500c6e3a8a1e06884ab3f0058dbfde3
WillDoyle <64522578+WillDoyle@users.noreply.github.com>
2022-01-27 19:39:39 +1000

    Rainforest slides finished, font upload and new sections

--- main.js
+++ main.js
@@ -8,14 +8,70 @@ var climateImg = document.getElementById("climate-image").innerHTML;
 //communitiesImg.style.opacity = "0.5";
 //climateImg.style.opacity = "0.5";
 
+AOS.init();
+
 
 function toggleVisibility(id) {
         $("figure#communities-image").css("opacity", id === "communities-image" ? 1 : 0)
-        
+
         $("figure#rainforest-image").css("opacity", id === "rainforest-image" ? 1 : 0);
 
         $("figure#climate-image").css("opacity", id === "climate-image" ? 1 : 0);
-    
+
         $("figure#default-image").css("opacity", id === "default-image" ? 1 : 0);
-    
+
+}
+
+function changeColor(id, hover) {
+
+        if (id == "communities-image") {
+                var targetDiv = document.getElementById("rainforest__3--sections").getElementsByClassName("communities-text")[0];
+                console.log(targetDiv.className);
+                var targetText = document.getElementById("rainforest__3--sections").getElementsByClassName("communities-para")[0];
+                var targetIcon = document.getElementById("rainforest__3--sections").getElementsByClassName("communities-icon")[0];
+        }
+
+        if (id == "rainforest-image") {
+                var targetDiv = document.getElementById("rainforest__3--sections").getElementsByClassName("rainforest-text")[0];
+                console.log(targetDiv.className);
+                var targetText = document.getElementById("rainforest__3--sections").getElementsByClassName("rainforest-para")[0];
+                var targetIcon = document.getElementById("rainforest__3--sections").getElementsByClassName("rainforest-icon")[0];
+
+        }
+
+        if (id == "climate-image") {
+                var targetDiv = document.getElementById("rainforest__3--sections").getElementsByClassName("climate-action-text")[0];
+                console.log(targetDiv.className);
+                var targetText = document.getElementById("rainforest__3--sections").getElementsByClassName("climate-action-para")[0];
+                var targetIcon = document.getElementById("rainforest__3--sections").getElementsByClassName("climate-icon")[0];
+
+        }
+
+        if (hover == 'leave') {
+                targetText.style.opacity = "0";
+                targetText.style.display = "none";
+                targetIcon.style.backgroundColor = "aliceblue";
+                if (targetDiv.className !== "communities-text") {
+                        targetDiv.style.color = "aliceblue";
+                        targetIcon.style.backgroundcolor = "aliceblue";
+                }
+        }
+        if (hover == 'enter') {
+                targetText.style.opacity = "1";
+                targetText.style.display = "block";
+                if (targetDiv.className !== "communities-text") {
+                        targetDiv.style.color = "#2B443C";
+                        targetText.style.color = "#2B443C";
+                        
+                }
+                
+                
+                if(targetDiv.className !== "climate-action-text"){
+                        targetIcon.style.backgroundColor = "#CFFF4F";
+                }
+                
+        }
+
+
+
 }
